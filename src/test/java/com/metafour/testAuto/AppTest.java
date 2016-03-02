7ebddc490cdf0831ef3296f59c2b51a03d31b7c2
sanjoy-roy-m4 <sanjoy.roy@metafour.com>
2016-03-02 12:33:54 +0600

    add Selenium test cases

--- src/test/java/com/metafour/testAuto/AppTest.java
+++ src/test/java/com/metafour/testAuto/AppTest.java
@@ -1,38 +1,82 @@
 package com.metafour.testAuto;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://192.168.0.175/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+  @Test
+  public void test01() throws Exception {
+    driver.get("http://ncpg-test1.metafour.lan/online/");
+    assertTrue(isElementPresent(By.xpath("//form[@id='mainform']//h2[contains(text(),'Login')]")));
+    assertTrue(isElementPresent(By.id("access_code")));
+    assertTrue(isElementPresent(By.id("login")));
+    driver.findElement(By.id("access_code")).clear();
+    driver.findElement(By.id("access_code")).sendKeys("TESTCL01");
+    driver.findElement(By.id("login")).clear();
+    driver.findElement(By.id("login")).sendKeys("testus01");
+    driver.findElement(By.name("j_password")).clear();
+    driver.findElement(By.name("j_password")).sendKeys("testus01");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    assertEquals("Online booking", driver.getTitle());
+    assertTrue(isElementPresent(By.xpath("//section[@id='booking']")));
+    assertTrue(isElementPresent(By.xpath("//section[@id='addresses']")));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
     }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+//  private boolean isAlertPresent() {
+//    try {
+//      driver.switchTo().alert();
+//      return true;
+//    } catch (NoAlertPresentException e) {
+//      return false;
+//    }
+//  }
+
+//  private String closeAlertAndGetItsText() {
+//    try {
+//      Alert alert = driver.switchTo().alert();
+//      String alertText = alert.getText();
+//      if (acceptNextAlert) {
+//        alert.accept();
+//      } else {
+//        alert.dismiss();
+//      }
+//      return alertText;
+//    } finally {
+//      acceptNextAlert = true;
+//    }
+//  }
 }

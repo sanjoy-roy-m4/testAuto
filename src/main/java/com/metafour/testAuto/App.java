280d749d8cd591f97aa1e8afb742c69de6e5e5a3
sanjoy-roy-m4 <sanjoy.roy@metafour.com>
2016-03-02 12:19:23 +0600

    first commit

---
+++ src/main/java/com/metafour/testAuto/App.java
@@ -0,0 +1,13 @@
+package com.metafour.testAuto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
